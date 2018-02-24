ecdd26b6beb41e44134f34b12e5bfc18171504c7
Jakub Kilar <kuba.kilar@gmail.com>
2018-02-24 12:09:24 +0100

    add triangle area script

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var heightTriangle = prompt('Podaj wysokość trójkąta');
+var baselenghtTriangle = prompt('Podaj długość podstawy trójkąta');
+var triangleArea = heightTriangle*baselenghtTriangle / 2;
+alert('Pole trójkąta wynosi ' + triangleArea + '.');
+console.log('Pole trójką z podstawą ' + baselenghtTriangle + ' i wysokością ' + heightTriangle + ' wynosi ' + triangleArea + '.');
